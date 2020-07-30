3fc6efdac8cfd2b0c9ebff0ef71dfe479f989a23
David <david.krygier1010@gmail.com>
2020-07-30 13:56:11 +0200

    MAJ fichier trajet_vacances

--- trajetVacances.py
+++ trajetVacances.py
@@ -2,47 +2,37 @@ import folium
 from numpy import random
 import psycopg2
 import pandas as pd
+import os
+import sys
 
-try:
-    connexion = psycopg2.connect(
+connexion = psycopg2.connect(
         database="vacances",
         user="masterUsername",
         password="SqualeApollo7",
         host="rds-postgresql-vacances.cvr2tfzdfp0j.eu-west-3.rds.amazonaws.com",
-        port='5432'
-    )
+        port='5432')
 
     #create_tables()
 
-    curseur = connexion.cursor()
+cur = connexion.cursor()
 
-    # Création des tables une à une
-    curseur.execute("""SELECT total_trajet.id_total, trajet.inseedepart, trajet.inseearrivee, trajet.emis_co2, trajet.duree, ville.longitude, ville.latitude
+base_dir = os.path.dirname(sys.argv[0])
+
+path = os.path.join(base_dir, "trajetVacancesduree.html")
+path2 = os.path.join(base_dir, "trajetVacancesco2.html")
+
+cur.execute("""SELECT ville.latitude, ville.longitude
                     FROM trajet, total_trajet, trajet_ville, ville
                     WHERE trajet.id = total_trajet.id_trajet
-                    AND	trajet.id = trajet_ville.id_trajet
-                    AND	ville.id = trajet_ville.id_ville
+                    AND    trajet.id = trajet_ville.id_trajet
+                    AND    ville.id = trajet_ville.id_ville
+                    and total_trajet.id_total = '1'
                     ORDER BY ville.longitude, ville.latitude;""")
 
-    rtRq5 = curseur.fetchall()
-    
-    # Afficher le résultat
-    print("\nRésultat: \n")
-    for ligne in rtRq5:
-        print(ligne[0], " | ", ligne[1])
+rqduree = cur.fetchall()
 
-    # fin de connexion avec PostgreSQL
-    curseur.close()
-    # commiter les modifications
-    connexion.commit()
 
-except (Exception, psycopg2.DatabaseError) as error:
-    print(error)
-finally:
-    if connexion is not None:
-        connexion.close()
-
-data = pd.DataFrame(rtRq5) # Convertit le résultat de la requete en DataFrame
+data = pd.DataFrame(rqduree) # Convertit le résultat de la requete en DataFrame
 
 # On convertit le DataFrame en martices
 matrice = data.to_numpy()
@@ -66,4 +56,44 @@ for each in matrice:
 folium.PolyLine(matrice, color="green", weight=2.5, opacity=1).add_to(my_map)
  
 # Save map
-my_map.save("./trajetVacances.html")
+my_map.save(path)
+
+
+
+# Création des tables une à une
+cur.execute("""SELECT ville.latitude, ville.longitude
+                    FROM trajet, total_trajet, trajet_ville, ville
+                    WHERE trajet.id = total_trajet.id_trajet
+                    AND    trajet.id = trajet_ville.id_trajet
+                    AND    ville.id = trajet_ville.id_ville
+                    and total_trajet.id_total = '2'
+                    ORDER BY ville.longitude, ville.latitude;""")
+
+rqco2 = cur.fetchall()
+
+
+data = pd.DataFrame(rqco2) # Convertit le résultat de la requete en DataFrame
+
+# On convertit le DataFrame en martices
+matrice = data.to_numpy()
+
+# Load map centred on average coordinates
+ave_lat = sum(p[0] for p in matrice)/len(matrice)
+ave_lon = sum(p[1] for p in matrice)/len(matrice)
+my_map = folium.Map(location=[ave_lat, ave_lon], zoom_start=6)
+
+#add a markers 'a'
+for each in matrice:  
+    my_map.add_child(folium.CircleMarker(location=each,
+    fill='true',
+    radius = 6,
+    popup= 'Hi',
+    fill_color='blue',
+    color = 'clear',
+    fill_opacity=1))
+    
+# add lines
+folium.PolyLine(matrice, color="green", weight=2.5, opacity=1).add_to(my_map)
+ 
+# Save map
+my_map.save(path2)
